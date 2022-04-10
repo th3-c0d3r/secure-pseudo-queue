5761197a4d5942eb0b3a042be4786dc281e8df91
Karthikeyan <emailtokarthi98@gmail.com>
2022-04-11 03:49:54 +0530

    added dev profile

--- src/main/java/com/example/demo/service/impl/SenderServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SenderServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.demo.utils.EncryptDecryptUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -21,13 +22,18 @@ public class SenderServiceImpl implements SenderService {
   private final RestTemplate restTemplate = new RestTemplate();
   private final ObjectMapper mapper = new ObjectMapper();
 
+  @Value("${app.base.url}")
+  private String baseUrl;
+
   @Override
   public void send(Object obj) throws JsonProcessingException {
     log.info("Object to send: {}", obj);
     String transactionalObjStr = EncryptDecryptUtils.encrypt(mapper.writeValueAsString(obj), "secret");
     log.info("Encrypt: {}", transactionalObjStr);
+
+    // Instead of making rest call, receive() in ReceiveService can be called directly
     HttpEntity<?> request = new HttpEntity<>(EncryptDto.builder().encryptedData(transactionalObjStr).build());
-    restTemplate.exchange("http://localhost:8080/receive/",
+    restTemplate.exchange(baseUrl + "/receive/",
       HttpMethod.POST, request, ApiResponse.class);
   }
 }
